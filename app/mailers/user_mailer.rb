1c1d84a22dc9a566b48b1bf4a83975b7ff0fcf1d
Kevin Paniccia <kevin.paniccia@gmail.com>
2018-07-21 00:54:48 +0000

    added email functionality

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,11 @@
+class UserMailer < ApplicationMailer
+  default from: 'no-reply@jungle.com'
+ 
+  def receipt_email(order)
+    @order = order
+    @url  = 'http://example.com/login'
+    mail(to: @order.email, subject: 'Welcome to My Awesome Site')
+  end
+
+
+end
